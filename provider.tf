a20e135bc5d86189424fa69f59ac487aa9247356
packdhe <57749945+packdhe@users.noreply.github.com>
2023-01-11 22:35:42 +0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,8 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = file(var.credentials)
  project     = var.project
  region      = var.region
 }
 
 variable "project" {}
 variable "region" {}
-variable "credentials" {}
